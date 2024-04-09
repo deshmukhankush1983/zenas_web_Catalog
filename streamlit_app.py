a958d436d6373bbde6b58e25b6eb7da3bf59515a
deshmukhankush1983 <165218490+deshmukhankush1983@users.noreply.github.com>
2024-04-09 16:54:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,4 @@ try:
   streamlit.text("Hello from Snowflake:")
   streamlit.text(my_data_row)
 except Exception as  e:
-   streamlit.text("DB connection Error")
+   streamlit.text("DB connection Error "+e)
